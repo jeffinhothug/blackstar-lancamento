5119da758b0785874938ea56a26e07e2bd91e275
Jeffinho Thug <ask.jeffinho@gmail.com>
2026-01-31 00:11:11 -0300

    feat: switch to text logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logoSrc from '../assets/logo.png';
 
 export const Logo: React.FC<{ className?: string }> = ({ className = '' }) => {
   return (
-    <img
-      src={logoSrc}
-      alt="Black Star Produtora"
-      className={`object-contain ${className}`}
-    />
+    <div className={`font-bold text-xl tracking-wider text-gold ${className}`}>
+      BLACKSTAR <span className="text-white">LANÇAMENTOS</span>
+    </div>
   );
 };
